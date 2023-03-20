29e08dbf98f3ef966cd80da494655946c396f14d
2923swapna <120497760+2923swapna@users.noreply.github.com>
2023-03-20 13:33:12 +0530

    Add files via upload

---
+++ Function.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+const Function=()=>{
+    return(
+        <h1>This is a function based comp</h1>
+    )
+}
+export default Function;
---
+++ Function2.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+const Swap=()=>{
+    return(
+        <div>jeevan </div>
+    )
+}
+export default Swap
